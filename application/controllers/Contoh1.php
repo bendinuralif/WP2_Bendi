890582a1451ed7001cc1a77f9b151013a572e70e
bendinuralif <bendipenthol@gmail.com>
2022-09-23 14:29:57 +0700

    Menambahkan Contoh 1

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama Saya Bendi Nur Alif. Saya tinggal di daerah Sragen olah raga yang saya sukai adalah Tidur";
+        echo"Nama Saya Bendi Saya tinggal di daerah Sragen olah raga yang saya sukai adalah Tidur";
     }
 }
