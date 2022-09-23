81aff8f0968be7abfdfc9ca0b4725e2e9b02743d
bendinuralif <bendipenthol@gmail.com>
2022-09-23 15:07:45 +0700

    Jijal

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h2>Perkenalkan</h2>";
-        echo"Nama Saya Bendi Saya tinggal di daerah Sragen olah raga yang saya sukai adalah Bulutangkis";
+        echo"Nama Saya Bendi Saya tinggal di daerah Sragen olah raga yang saya sukai adalah Volly";
     }
 }
